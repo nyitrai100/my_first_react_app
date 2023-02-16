0edc715b7e5367d6ca75a81ceefac92e78681ce4
nyitrai100 <nyitrai100@gmail.com>
2023-02-16 20:35:27 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const name = 'Jane';
+  const isNameShowing= false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello, {isNameShowing ? name : 'someone'}!</h1>
+    {name ? (
+    <>
+    <h1>{name}</h1>
+    
+    </>
+    ) : (
+    <> 
+    <h1>test</h1>
+    <h2>There is no name</h2>
+    </>
+  )}
     </div>
   );
 }
