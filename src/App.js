9dce199262ed2f3adbe4cda750f2602a61b633f4
nyitrai100 <nyitrai100@gmail.com>
2023-02-17 15:37:04 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import{useState} from 'react'
+import{useState, useEffect} from 'react'
 import './App.css';
 
 
 const App = () => {
 
   const [counter, setCounter] = useState(0);
+
+  useEffect(() =>{
+    setCounter(100);
+  })
 
   return (
     <div className="App">
