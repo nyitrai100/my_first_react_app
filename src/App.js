e22fce73a0282eff0095a350d1fa468fe8deffac
nyitrai100 <nyitrai100@gmail.com>
2023-02-17 15:31:26 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-
+import{useState} from 'react'
 import './App.css';
 
-const Person = (props) => {
-  return(
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.LastName}</h2>
-    <h2>Age: {props.Age}</h2>
-    </>
-  )
-}
 
 const App = () => {
+
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-     < Person
-         name='John'
-         LastName={'Doe'}
-          Age={'25'}
-          />
-
-     < Person 
-        name="Mary" 
-         LastName={'Doe'} 
-         Age={'25'}
-         />
-
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
