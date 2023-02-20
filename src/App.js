a82fd54799261be1e73d4da096aabf6996910f6b
nyitrai100 <nyitrai100@gmail.com>
2023-02-20 16:34:46 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import { useEffect } from "react";
 
 
+const API = 'http://www.omdbapi.com/?i=tt3896198&apikey=5750106e';
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API}&s=${title}`);
+        const data = await response.json();
+        console.log(data.search);
+    }
+
+    useEffect(() =>{
+    searchMovies('Spiderman');
+
+    }, []);
+
     return(
         <h1>App</h1>
     );
