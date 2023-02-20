dd7cbe44b025f12f709275c2f97759b17ea58ff0
nyitrai100 <nyitrai100@gmail.com>
2023-02-20 16:51:48 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { useEffect } from "react";
 
+import './app.css';
+
+import SearchIcon from './search.svg';
+
 
 const API = 'http://www.omdbapi.com/?i=tt3896198&apikey=5750106e';
+
+const movie1={
+    "Title": "Amazing Spiderman Syndrome",
+    "Year": "2012",
+    "imdbID": "tt2586634",
+    "Type": "movie",
+    "Poster": "N/A"
+}
+
 const App = () => {
 
     const searchMovies = async (title) => {
@@ -16,7 +29,41 @@ const App = () => {
     }, []);
 
     return(
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+
+                <input
+                    placeholder="Search for movies"
+                    value="Superman"
+                    onChange={() => {}}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+
+            <div Name= "container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     );
 }
 
